11664d127b23ded1c26bd579478eb8d85a5259f6
YlanDecode <rabenaly3@gmail.com>
2025-12-18 20:18:15 +0300

    feat: add prod p2m

---
+++ ecosystem.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [{
+    name: 'fox-reviews-front',
+    script: 'serve',
+    args: 'dist -s -l 3000',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
+    time: true
+  }]
+};
